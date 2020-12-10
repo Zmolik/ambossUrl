918150b5b536c8975a9d80e658f1bf9370c07ead
Zmolik <62994120+Zmolik@users.noreply.github.com>
2020-12-10 11:08:55 +0100

    Update searchUrlByName.ts

--- searchUrlByName.ts
+++ searchUrlByName.ts
@@ -12,15 +12,15 @@ fs.readFile('.\\json-products\\diagnosis-list-updated2.json', 'utf-8', (err, dat
 
     //let list = [];
 /*
-Problem: If I tried to loop over the whole json file (1375 elements), it would crash. I tried other to scrape the links by myself
+Problem: If I tried to loop over the whole json file (1375 elements), it would crash. I tried to scrape the links by myself
 but I wasn't succesful. Search-engine-client library was the only library I could make work, but it works only on small amount of
 searches. So I did it half-manually. Via trial/error I realised that the best number of concurrent searches is 30. 
 Solution: So I changed manually the variable "mal" from 1 to 45 (when mal == 45 then you have to change var "to" = 1375) and got 
 like 90 percent of success. Still there were some empty searches (urlByName = []) so I loop over it again, this time over 200 at once,
-I used if condition to not search again for the once that have already links saved. Through repeating this cca 5 time for each 200
-I got to find the links where it was possible. There are still some without any links in urlByName, but there are no search results
-on duckduckgo. 
-I think this can work without the manual part but there has to be some time delay implemented but I didn't know how.
+I used if condition (in this code the if condition is deleted) to not search again for the once that have already links saved.
+Through repeating this cca 5 time for each 200 I got to find the links where it was possible. There are still some without any links 
+in urlByName, but there are no search results on duckduckgo. 
+I think this can work without the manual part but there has to be some time delay implemented between each iteration but I didn't know how.
 */
 
     let mal = 40  
